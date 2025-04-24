a157e684440aeb1ad680b2dc7ad81bb35623e2aa
Yauhak <yauhak_2024@qq.com>
2025-04-24 20:31:35 +0800

    Update INTERPRETER.h

--- INTERPRETER.h
+++ INTERPRETER.h
@@ -14,6 +14,9 @@
 	#define NEED_APPEND_TO_TAIL 8
 	#define MEM_CLEAN_PARTLY 9
 	#define NO_FREE_MEM 10
+	#define NO_MEM_TAIL 11
+	#define NO_MEM_HEAD 12
+	#define BAD_FREE_BLOCK 13
 
 	#define SPLIT "SPLT" //魔术字：已被程序占位
 	#define FREE "FREE" //魔术字：程序内存已被清空
@@ -27,6 +30,7 @@ struct ParamStack {
 		int8_t BYTE;
 		int16_t DWORD;
 		int32_t INT;
+		float FLOAT;
 	} DATA __attribute__((aligned(4))); // 4字节对齐
 };
 
@@ -42,6 +46,8 @@ struct Magic {
 	//最后的内存防线
 	//若连此值都被破坏则认为该段内存完全损坏
 	uint32_t len;
+	//len=数据块长度+3字节缓冲
+	//不包括魔术字头
 	volatile uint8_t *last_block;
 	volatile uint8_t *next_block;
 	//注意
@@ -70,10 +76,13 @@ typedef struct Magic Magic;
 typedef enum {
 	PCHR = 0x00, // 输出一个字符（参数为立即数或地址）
 	PSTR,// 输出一个字符串（参数为地址，输出直到内容为'\0'）
+	PVAL,//输出数值（DWORD,INT,FLOAT,参数为立即数或地址）
 	KEYINPUT, // 从键盘接收消息，存放到参数所表示的地址中
+	VALINPUT,//从键盘输入数值
 	MOV_BYTE, // 将参数2（立即数或地址，大小为一个字节）存入参数1表示的地址内存
 	MOV_DWRD, // 将参数2（立即数或地址，大小为二个字节）存入参数1表示的地址内存
 	MOV_INT, // 将参数2（立即数或地址，大小为四个字节）存入参数1表示的地址内存
+	MOV_FLOAT,//将参数2（立即数或地址，FLOAT类型）存入参数1表示的地址内存
 	PUSH,//将CalcResu的值保存于参数表示的地址中
 	PUSHP,//为子程序传入参数
 	EQ, // 判断参数1 == 参数2，结果存CalcResu
@@ -100,14 +109,17 @@ int16_t HigherInt32(int x);
 int16_t LowerInt32(int x);
 int16_t Tran8To16(int8_t higher, int8_t lower);
 int Tran16To32(int16_t higher, int16_t lower);
-void ReadByteMem(uint8_t *Recv);
-int8_t findByteWithAddr();
-int16_t findDByteWithAddr();
-int findIntWithAddr();
-void setByte(int8_t byteText);
-void setDByte(int16_t DbyteText);
-void setInt(int32_t intText) ;
-int8_t ReArrangeMemAndTask(uint8_t id,uint8_t isForce);
+//内存读写需要考虑多任务情况
+//每个函数应显式要求提供程序ID
+//以防止时间片轮转后ID切换，导致读取其他程序的内存
+void ReadByteMem(uint8_t *Recv,uint8_t id);
+int8_t findByteWithAddr(uint8_t id);
+int16_t findDByteWithAddr(uint8_t id);
+int findIntWithAddr(uint8_t id);
+void setByte(int8_t byteText,uint8_t id);
+void setDByte(int16_t DbyteText,uint8_t id);
+void setInt(int32_t intText,uint8_t id) ;
+int8_t ReArrangeMemAndTask(uint8_t id);
 int8_t DelLastFuncMem(uint8_t id);
 int8_t SuperFree(Magic *block);
 int findFreeMemById(uint8_t id, int allocLen, int level);
