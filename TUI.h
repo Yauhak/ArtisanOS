d0dcec599cd9d6c622e10b298ba157973d3028e2
Yauhak <yauhak_2024@qq.com>
2025-04-29 21:32:18 +0800

    Update TUI.h

--- TUI.h
+++ TUI.h
@@ -1,4 +1,9 @@
-#ifndef TUI
-	#define TUI
+#ifndef IO_INCLUDE
 	#include "IO_INCLUDE.h"
 #endif
+#ifndef TUI
+	#define TUI
+#endif
+
+void draw_title(char *str, uint8_t lenStr);
+void mark_line(uint8_t line);
