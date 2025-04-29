744ea13e3c7030c6e7691bd77f3c48174c097d18
Yauhak <yauhak_2024@qq.com>
2025-04-29 21:30:44 +0800

    Update INTERPRETER.h

--- INTERPRETER.h
+++ INTERPRETER.h
@@ -10,9 +10,12 @@
 struct menu{
 	uint8_t is_registered;//是否注册了菜单结构
 	uint8_t id;//哪个程序需要启用菜单结构
+	uint8_t handle;//这里相当于菜单的ID
 	uint8_t menu_len;//菜单项目数
 	uint8_t *menu_prompt[MENU_MAXSIZE];//菜单最大32个选项，该指针指向不同选项提示语句的地址
-};
+	uint32_t action[MENU_MAXSIZE];//每个选项对应的动作（函数）
+}__attribute__((packed));
+typedef struct menu menu;
 
 /*
 	该部分为“天工”操作系统的程序执行逻辑部分
@@ -55,10 +58,34 @@ typedef enum {
 	SUB, // 减法
 	MUL, // 乘法
 	DIV, // 除法
+	MENU_HANG,//挂载菜单
+	MENU_REG,//注册菜单选项
+	MENU_SHOW,//显示菜单
 	HLT, // 程序结束标记
 } Opcode;
 
 //内存读写需要考虑多任务情况
 //每个函数应显式要求提供程序ID
 //以防止时间片轮转后ID切换，导致读取其他程序的内存
+int8_t pchr(uint8_t ParamType, int32_t *params, uint16_t taskId);
+int8_t pstr(uint8_t ParamType, int32_t *params, uint16_t taskId);
+int8_t pval(uint8_t ParamType, int32_t *params, uint16_t taskId);
+int8_t key_input(uint8_t ParamType, int32_t *params, uint16_t taskId);
+int8_t val_input(uint8_t ParamType, int32_t *params, uint16_t taskId);
+int8_t _read_file(uint8_t ParamType, int32_t *params, uint16_t taskId);
+int8_t _write_file(uint8_t ParamType, int32_t *params, uint16_t taskId);
+int8_t _del_file(uint8_t ParamType, int32_t *params, uint16_t taskId);
+int8_t mov(uint8_t ParamType, int32_t *params, uint16_t taskId);
+int8_t push(uint8_t ParamType, int32_t *params, uint16_t taskId);
+int8_t pushp(uint8_t ParamType, int32_t *params, uint16_t taskId);
+int8_t call(uint8_t ParamType, int32_t *params, uint16_t taskId);
+int8_t ret(uint8_t ParamType, int32_t *params, uint16_t taskId);
+int8_t conds(uint8_t cmdAndPmTp, int32_t *params, uint16_t taskId);
+int8_t jmp(uint8_t ParamType, int32_t *params, uint16_t taskId);
+int8_t jmp_t(uint8_t ParamType, int32_t *params, uint16_t taskId);
+int8_t calc(uint8_t cmdAndPmTp, int32_t *params, uint16_t taskId);
+int8_t menu_hang(uint8_t ParamType, int32_t *params, uint16_t taskId);
+int8_t menu_reg(uint8_t ParamType, int32_t *params, uint16_t taskId);
+int8_t menu_show(uint8_t ParamType, int32_t *params, uint16_t taskId);
+int8_t hlt(uint8_t ParamType, int32_t *params, uint16_t taskId);
 int8_t interprete(uint8_t cmd, int32_t *params, uint16_t taskId);
